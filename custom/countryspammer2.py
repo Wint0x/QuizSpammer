8fe7de84f07e135e1446d8405cc847a0206eeb93
Zamius <111395278+Zamius@users.noreply.github.com>
2022-08-17 03:23:35 +0300

    Create countryspammer2.py
    Include file "example.txt", file must contain your quiz answers separated by comma.

---
+++ custom/countryspammer2.py
@@ -0,0 +1,31 @@
+from time import perf_counter, sleep as s
+from pyautogui import typewrite as spammer
+import sys
+
+
+START = perf_counter()
+spammer.PAUSE = 0
+
+def get_list():
+    #Get file content in form of list, note that the file must contain strings separated by comma.
+    with open('example.txt', 'r') as f:
+        answers = f.read()
+        answers = list(map(lambda x: x.replace("\n",""), answers.split(',')))
+    return answers
+
+#debug
+
+
+print("You have 5 seconds to find find your quiz's entry box")
+print(f"\nSending: {len(get_list()) + 1} words!\n")
+
+s(5)
+
+answers = get_list()
+
+for word in answers:
+    spammer(word,interval=0)
+
+FINISH = perf_counter()
+
+print(f"Finished in: {round(FINISH-START,2)} seconds!")
